28cdabf439bc0a2b3f1769f4a48587f4e39912bf
someoneuk <aryoyudhaalt@gmail.com>
2026-08-25 14:48:15 +0700

    Refactor button, toggle, and input creation syntax

--- Libraries/RJelly/examplescript.lua
+++ Libraries/RJelly/examplescript.lua
@@ -1,37 +1,49 @@
-local RJelly = loadstring(Game:HttpGet("https://raw.githubusercontent.com/someoneuk/Warehouse/refs/heads/main/Libraries/RJelly/RJellyUI.lua"))()
+local RJelly = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneuk/Warehouse/refs/heads/main/Libraries/RJelly/RJellyUI.lua"))()
 
 local Window = RJelly:MakeWindow("MyWindow")
 local Tab = Window:MakeTab("MyTab")
 
-Tab:MakeButton("Click me!",function() -- BUTTON
-  -- Here you can run codes
-  print('do something')
+local Button = Tab:MakeButton("Click me!",function() -- BUTTON
+	-- Here you can run codes
+	print('do something')
 end)
 
-Tab:MakeToggle("Toggle",function(value) -- TOGGLE
-  -- Here you can run codes
-  -- Value is either true/false
+local Toggle = Tab:MakeToggle("Toggle",function(value) -- TOGGLE
+	-- Here you can run codes
+	-- Value is either true/false
 
-  if value == true then
-    print("Toggle on")
-  else
-    print("Toggle off")
-  end
+	if value == true then
+		print("Toggle on")
+	else
+		print("Toggle off")
+	end
 end)
 
-Tab:MakeInput("Print ANYTHING",function(input, enterPressed) -- TEXTBOX
-  if enterPressed then
-    print("you typed "..input.." and pressed enter!")
-  else
-    print("you typed "..input.." without pressing enter!")
-  end
+local Input = Tab:MakeInput("Print ANYTHING",function(input, enterPressed) -- TEXTBOX
+	if enterPressed then
+		print("you typed "..input.." and pressed enter!")
+	else
+		print("you typed "..input.." without pressing enter!")
+	end
 end)
 
 local Dropdown = Tab:MakeDropdown("Dropdown Prints") -- DROPDOWN
 Dropdown:AddOption("Hello",function()
-  print("Hello")
+	print("Hello")
 end)
 
 Dropdown:AddOption("Goodbye",function()
-  print("Goodbye")
+	print("Goodbye")
 end)
+
+Tab:MakeLabel("Header") -- LABEL
+Tab:MakeSpace(15) -- SPACE, just a gap, doesn't need a variable
+
+-- every element gives you back a module now, so you can control it after making it
+-- here's a couple examples using the Button and Toggle we made above
+
+Button:SetTitle("Click me again!")
+
+if Toggle:GetValue() == false then
+	Toggle:SetNextActivationValue(true) -- next time it's clicked, it'll turn on
+end
